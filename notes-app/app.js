bbbc205f8b2bd456888a91a63ff15a46cfcbd2ed
Hrishabh Jain <hrishabh507@gmail.com>
2021-10-16 00:53:24 +0530

    S3 V6

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,5 @@ const validator = require("validator");
 const chalk = require("chalk");
 const getNotes = require("./notes");
 
-console.log(validator.isEmail("hrishabh507@gmail.com"));
-console.log(validator.isURL("https:/gmail.com"));
 console.log(getNotes());
-console.log("npm " + chalk.bold.bgYellow.black("WARN"));
+console.log("npm " + chalk.bgRed.black("ERR"));
