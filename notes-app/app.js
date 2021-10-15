807153a288aea479e1fa9e4d51f9a56d0baaf53f
Hrishabh Jain <hrishabh507@gmail.com>
2021-10-15 23:29:41 +0530

    Initial Commit

---
+++ notes-app/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+fs.writeFileSync("notes.txt", "My name is Hrishabh Jain.");
+
+/*
+
+Challenge: Append a message to notes.txt
+
+1. Use appendFileSync to append to the file
+2. Run the script
+3. Check your work by opening the file and viewing the appended text.
+
+*/
+
+fs.appendFileSync("notes.txt", " How are you, bro?");
