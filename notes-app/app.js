6a49d09c361c4afc2ecbc5643bf355a02a644896
Hrishabh Jain <hrishabh507@gmail.com>
2021-10-16 01:51:48 +0530

    S4 V3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,45 @@
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-const command = process.argv[2];
-if (command === "add") {
-  console.log("Adding a note!");
-} else if (command === "remove") {
-  console.log("Removing a note!");
-}
+// console.log(process.argv);
+
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note..");
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing a note..");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List all the notes",
+  handler: function () {
+    console.log("Listing out all the notes..");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading out a note..");
+  },
+});
+
+console.log(yargs.argv);
