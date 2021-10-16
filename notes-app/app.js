ee2a1ea88dee5afbc4f50c6bc0578736e1b2197b
Hrishabh Jain <hrishabh507@gmail.com>
2021-10-16 11:32:11 +0530

    S4 V4

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,8 +10,21 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  handler: function () {
-    console.log("Adding a new note..");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    description: {
+      describe: "Note description",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Description: " + argv.description);
   },
 });
 
@@ -42,4 +55,6 @@ yargs.command({
   },
 });
 
-console.log(yargs.argv);
+// console.log(yargs.argv);
+
+yargs.parse();
