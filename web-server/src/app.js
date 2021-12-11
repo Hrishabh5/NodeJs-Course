4e8d77166c600b9643c95c5162e04ab6b69942b1
Hrishabh Jain <hrishabh507@gmail.com>
2021-12-11 11:48:29 +0530

    S7 V8

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express(); // we are storing the complete express application in "app"
 
 // Define paths for express config
 const publicDir = path.join(__dirname, "..", "/public");
-const viewsDir = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
-app.set("views", viewsDir);
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDir));
@@ -31,8 +34,10 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    title: "Help",
     message:
       "I’m currently a CSE undergrad who loves to explore different fields. Artificial Intelligence is something which fascinates me a lot but since I had just started coding few months ago for the first time, I decided to learn the basics first with Java. At present, I’m learning Java and JS and now I know quite a bit in terms of basic concepts.",
+    name: "Hrishabh Jain",
   });
 });
 
