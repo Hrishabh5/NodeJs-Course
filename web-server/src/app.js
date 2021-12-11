b41a436ef06954dcf7c289443889b112d3652f47
Hrishabh Jain <hrishabh507@gmail.com>
2021-12-11 09:43:53 +0530

    S4 V5

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,24 +4,7 @@ const express = require("express");
 const app = express(); // we are storing the complete express application in "app"
 const publicDir = path.join(__dirname, "..", "/public");
 
-// app.com
-// app.com/help
-// app.com/about
-/*
-app.get("", (req, res))
-
--- "" -> (Route) Here we are gonna provide the partial url like "/help", "/about"
--- (req, res) -> This function basically tells what we wanna do when someone visits that route 
--- req -> (request) Contains info about the incoming requests
--- res -> (response) Contains a bunch of methods which will customize what we are gonna send back to the requester
-*/
-
 app.use(express.static(publicDir));
-
-// This will never be executed now
-// app.get("", (req, res) => {
-//   res.send("<h1>Weather</h1>");
-// });
 
 app.get("/weather", (req, res) => {
   res.send({
@@ -30,7 +13,6 @@ app.get("/weather", (req, res) => {
   });
 });
 
-// listen method starts up the server on port provided as the first argument
 app.listen(3000, () => {
   console.log("Server is up on port 3000!");
 });
