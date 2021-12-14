0f8a408a84b390f4dc957d75897752286f3d5496
Hrishabh Jain <hrishabh507@gmail.com>
2021-12-14 23:42:02 +0530

    S8 V6-7

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,13 +6,42 @@ console.log("Client side JS file is loaded!");
 //   });
 // });
 
-fetch("http://localhost:3000/weather?location=boston").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+// fetch("http://localhost:3000/weather?location=boston").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const forecast = document.querySelector(".forecast");
+const place = document.querySelector(".location");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  forecast.innerHTML = `<img class="loader" src="./img/loading.gif" alt="Loading...">`;
+  place.innerText = "";
+
+  fetch(`http://localhost:3000/weather?location=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          forecast.innerText = data.error;
+        } else {
+          forecast.innerText = data.forecast;
+          place.innerText = data.location;
+        }
+      });
     }
-  });
+  );
+
+  //   console.log(location);
 });
