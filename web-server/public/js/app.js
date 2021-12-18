b5da56ec0e1326eca9f833aad51674113c45a680
Hrishabh Jain <hrishabh507@gmail.com>
2021-12-18 16:54:46 +0530

    Wrapping up and deploying

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,5 @@
 console.log("Client side JS file is loaded!");
 
-// fetch("https://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-// fetch("http://localhost:3000/weather?location=boston").then((response) => {
-//   response.json().then((data) => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(data.forecast);
-//     }
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const forecast = document.querySelector(".forecast");
@@ -42,6 +25,4 @@ weatherForm.addEventListener("submit", (e) => {
       });
     }
   );
-
-  //   console.log(location);
 });
